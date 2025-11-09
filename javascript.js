7f51a668fd336de619e2efea31923066aa0544dd
Leo Mackenzie <leomackenzie2205@gmail.com>
2025-11-09 19:03:53 +0000

    Completed question 1

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//Question 1
+{
+    console.log('QUESTION 1:');
+
+    let myString = 'Leo';
+    let myBoolean = true;
+    let myNumber = 67;
+
+    console.log(`String: ${myString}`);
+    console.log(`Boolean: ${myBoolean}`);
+    console.log(`Number: ${myNumber}`);
+}
